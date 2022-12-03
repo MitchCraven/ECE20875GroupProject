6a629eb14b0c74d0f9957e2e6339d00939efa4d5
MitchCraven <craven5@purdue.edu>
2022-12-02 23:07:56 -0500

    Added Feature Matrix Menu and Split data into Training and Testing Set Methods

--- MiniProjectPath1.py
+++ MiniProjectPath1.py
@@ -20,7 +20,7 @@ dataset_1['Precipitation']  = pandas.to_numeric(dataset_1['Precipitation'].repla
 #print(shuffled.to_string()) #This line will print out your data
 
 # Break Down and Fix
-
+##Convert to numpy arrays
 brooklyn = dataset_1['Brooklyn Bridge']
 brooklyn = brooklyn.to_numpy()
 
@@ -54,8 +54,8 @@ avg_temp = np.divide((low_temp + high_temp), 2)
 
 # We are using 80% data for training.
 degrees = [1, 2, 3, 4, 5, 6, 7]
-a_list = np.array(list(range(1, round(len(brooklyn) * 0.8) + 1)))
-a_lis = np.array(list(range(round(len(brooklyn) * 0.8), len(brooklyn))))
+a_list = np.array(list(range(1, round(len(brooklyn) * 0.8) + 1))) #First 80% of data
+a_lis = np.array(list(range(round(len(brooklyn) * 0.8), len(brooklyn)))) #last 20% of data
 predict = [0] * len(degrees)
 models = [0] * len(array_data)
 degree_out = [0] * len(array_data)
@@ -88,4 +88,46 @@ for i in range(len(models)):
     plt.scatter(range_x, array_data[i])
     plt.show()
 
+#Train Test split splits a numpy array into 
+#data_list_variables is a list of variables, where each variable is its own numpy array of data that will be used
+#perc is the percent of the data we want to use
+#returns a pandas dataframe of the training and test data
+def TrainTestSplit(variablesDataList, perc):
+    
+    #this concatenates the variables you want into a pandas dataframe
+    #each column is a variable
+    data = variablesDataList[i]
+    for i in range(1, len(variablesDataList)):
+        data = pandas.concat([data, variablesDataList[i]], axis=0)
 
+    data = data.shuffle() #shuffles your data into training and test set. THIS MAKES YOUR MSE DIFFERENT EACH TIME
+    return data[:0.8*len(variablesDataList[i])] , data[0.8*len(variablesDataList[i]):]
+    #returns training data and testing data
+
+#makes a numpy array of a given degree k from input pandas dataframe
+def makeFeatureMatrix(trainingData, k):
+    
+    FM = np.array()
+
+    for i in trainingData.keys(): #takes column names
+        
+        x = trainingData.loc[:,i].to_numpy() #Converts column to numpy array
+
+        #new feature matrix
+        X = []
+        for i in x:
+            newX = []
+            for j in reversed(range(k+1)):
+              newX.append(i**j)
+            X.append(newX)
+    #adds this feature matrix into new feature matrix
+    FM = np.concatenate((FM, X), axis=0)
+    return FM
+
+#calculates least squares regression from feature matrix and numpy array of test data
+#returns numpy parameters
+def train_model(featureMatrix, yTrain):
+    B = np.linalg.inv(featureMatrix.T @ featureMatrix) @ featureMatrix.T @ yTrain
+    return B
+
+##REMEMBER DATATYPES AND MAKE SURE THERES A ONES COLUMN IN FEATURE MATRIX
